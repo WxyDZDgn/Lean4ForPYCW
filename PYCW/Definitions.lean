9b6758fe6d6f9cb62ca85d855c9ca01dc8912d9a
WxyDZDgn <279916012@qq.com>
2025-06-14 03:34:42 +0800

    完成rule1_2前的结构

--- PYCW/Definitions.lean
+++ PYCW/Definitions.lean
@@ -1,4 +1,6 @@
 import Mathlib.Data.Multiset.Basic
+import Mathlib.Data.List.Basic
+import Mathlib.Tactic.Linarith
 
 inductive Element where
 | nat: Nat → Element
@@ -9,29 +11,103 @@ open Element
 
 structure Chain where
   nodes: List Element
-  nonempty: nodes ≠ []
-  deriving Repr
+  length_geq_two: nodes.length ≥ 2
+  deriving DecidableEq, Repr
 
 def Chain.ends (c: Chain): Element × Element :=
   let lst := c.nodes
   (lst.head (by {
-    simp
-    exact c.nonempty
+    have h_len : c.nodes.length ≥ 2 := c.length_geq_two
+    apply List.length_pos_iff.mp
+    linarith
   }), lst.getLast (by {
-    simp
-    exact c.nonempty
+    have h_len : c.nodes.length ≥ 2 := c.length_geq_two
+    apply List.length_pos_iff.mp
+    linarith
   }))
 
 structure Level where
-  chains: Multiset Chain
+  chains: List Chain
+  length_geq_one: chains.length ≥ 1
+  deriving Repr
 
+def Level.neZero (level : Level) : NeZero level.chains.length :=
+  ⟨Nat.one_le_iff_ne_zero.mp level.length_geq_one⟩
 
 def Chain.validForAxiom1 (c: Chain): Bool :=
   let ends := c.ends
   ends = (nat 0, omega) || ends = (omega, nat 0)
 
+def Chain.splitAt (c: Chain) (q: Nat): Option (Chain × Chain) :=
+  let n := c.nodes.length
+  if h: 1 ≤ q ∧ q ≤ n - 2 ∧ 3 ≤ n then
+    let leftNodes := c.nodes.take (q + 1)
+    let rightNodes := c.nodes.drop q
+    some (⟨leftNodes, by {
+      simp [leftNodes]
+      have h_len : c.nodes.length ≥ 2 := c.length_geq_two
+      change 1 ≤ q ∧ 2 ≤ n
+      rcases h with ⟨h1, h2, h3⟩
+      have h2_l : 2 ≤ n := by linarith
+      exact ⟨h1, h2_l⟩
+    }⟩, ⟨rightNodes, by {
+      simp [rightNodes]
+      have h_len : c.nodes.length ≥ 2 := c.length_geq_two
+      rcases h with ⟨h1, h2, h3⟩
+      change 2 ≤ n - q
+      have h2_1 : 2 ≤ n := by linarith
+      have h2_2: q + 2 ≤ (n - 2) + 2 := Nat.add_le_add_right h2 2
+      rw [Nat.sub_add_cancel h2_1] at h2_2
+      apply Nat.le_sub_of_add_le
+      rw [Nat.add_comm]
+      exact h2_2
+    }⟩)
+  else
+    none
+
+def Chain.is_valid_sublist (sublist: List Element) (lst: List Element): Bool :=
+  let subChainLength := sublist.length
+  let chainLength := lst.length
+  if h_grater_than_two: subChainLength < 2 ∨ chainLength < 2 then false
+  else if subChainLength > chainLength then false
+  else
+    let res := sublist = lst.take subChainLength
+    if not res then
+      Chain.is_valid_sublist sublist lst.tail
+    else res
+termination_by lst.length
+decreasing_by
+  simp at h_grater_than_two
+  rcases h_grater_than_two.right with h_grater_than_two_2
+  have h_ne: lst ≠ [] := by {
+    intro h_nil
+    change 2 ≤ lst.length at h_grater_than_two_2
+    rw [h_nil] at h_grater_than_two_2
+    contradiction
+  }
+  cases lst with
+  | nil => contradiction
+  | cons a as => {
+    simp only [List.tail, List.length]
+    apply Nat.lt_succ_self
+  }
+
+def Chain.removeChain (level: Level) (subchain_idx: Fin level.chains.length) (chain_idx: Fin level.chains.length): Level :=
+  if h_stay_same: (subchain_idx = chain_idx ∨ ¬Chain.is_valid_sublist (level.chains.get subchain_idx).nodes (level.chains.get chain_idx).nodes) then level
+  else
+    let subchain := level.chains.get subchain_idx
+    -- let chain := level.chains.get chain_idx
+    let chains := level.chains.erase subchain
+    Level.mk chains (by {
+      simp at h_stay_same
+      rcases h_stay_same with ⟨h_idx_neq, h_valid_sub⟩
+      sorry
+    })
+
 inductive Beatable: Level → Prop where
-| apply_axiom1 (c: Chain) (h: c.validForAxiom1 = true): Beatable (Level.mk [c])
+| axiom1 (c: Chain) (h: c.validForAxiom1 = true): Beatable (Level.mk [c] (by simp))
+| rule1_1 {level: Level} (newChain: Chain) (h: Beatable level): Beatable (Level.mk (level.chains ++ [newChain]) (by simp))
+| rule1_2 {level: Level} (subchain_idx chain_idx: Fin level.chains.length) (h_level: Beatable level): Beatable (Chain.removeChain level subchain_idx chain_idx)
 
 syntax "(" term,+ "⟩" : term
 macro_rules
@@ -39,9 +115,9 @@ macro_rules
     let listExpr := ← `([$es,*])
     `(Chain.mk $listExpr (by decide))
 
-instance : Repr Chain where
-  reprPrec c _ :=
+instance : ToString Chain where
+  toString c :=
     let parts := c.nodes.map fun
       | nat n => toString n
       | omega => "ω"
-    "(" ++ (" -- ".intercalate parts) ++ "⟩"
+    ("--".intercalate parts)
--- PYCW/Tasks.lean
+++ PYCW/Tasks.lean
@@ -1,10 +1,31 @@
 import PYCW.Definitions
 
-theorem statement1: Beatable (
+-- 命题 1.1：证明关卡 {0--1--2--w} 可以过关
+theorem statement1_1: Beatable (
   Level.mk [
     (Element.nat 0, Element.nat 1, Element.nat 2, Element.omega⟩
-    ]
+    ] (by simp)
     ) := by {
-  apply Beatable.apply_axiom1
+  apply Beatable.axiom1
   decide
 }
+
+-- 命题 1.2：证明关卡 {2--1--0--w--3} 可以过关
+-- theorem statement1_2: Beatable (
+--   Level.mk [
+--     (Element.nat 2, Element.nat 1, Element.nat 0, Element.omega, Element.nat 3⟩
+--     ]
+--     ) := by {
+--   have h: Beatable (Level.mk [(Element.nat 0, Element.omega⟩] (by simp)) := by {
+--     apply Beatable.axiom1
+--     decide
+--   }
+--   apply Beatable.rule1_1 (Element.nat 2, Element.nat 1, Element.nat 0, Element.omega, Element.nat 3⟩ at h
+--   simp at h
+--   apply Beatable.rule1_2 (1:Fin 2) (1:Fin 2) h
+
+
+-- }
+
+-- #eval (Element.nat 0, Element.nat 1, Element.nat 2, Element.omega⟩
+#eval List.concat [1, 2, 3] 4
